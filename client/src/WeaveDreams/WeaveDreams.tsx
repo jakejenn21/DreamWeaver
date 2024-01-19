2495debdcbb955f4937e37f5e6f45f0c77194795
Jacob Jenn <jakejenn21@gmail.com>
2024-01-19 13:56:46 -0700

    add open ai to backend

--- client/src/WeaveDreams/WeaveDreams.tsx
+++ client/src/WeaveDreams/WeaveDreams.tsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./WeaveDreams.css";
 
+
 const WeaveDreams: React.FC = () => {
+  const [weaveData, setWeaveData] = useState<string>();
+
+  useEffect(() => {
+    fetchDreamWeaves();
+  }, []);
+
+  const fetchDreamWeaves = () => {
+    fetch("/api/dream/weave")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setWeaveData(data.message);
+      });
+  };
 
   return (
-    <></>
+    <>{weaveData}</>
   );
 };
 
--- server/server.js
+++ server/server.js
@@ -3,6 +3,11 @@ const cors = require('cors');
 const app = express();
 const MongoDBClient = require('./db/db-init');
 const { ObjectId } = require('mongodb');
+const OpenAI = require('openai');
+
+const openai = new OpenAI({
+  apiKey: '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',
+});
 
 const uri = "mongodb+srv://jakejenn21:dInx6hs3aMdP8zp5@dreamweavercluster.6pbawnn.mongodb.net/?retryWrites=true&w=majority";
 
@@ -25,11 +30,29 @@ app.get('/api/dream', async(req, res) => {
 });
 
 app.delete('/api/dream/:id', async(req, res) => {
-  console.log('DELETE')
   const { id } = req.params;
   const dreamCollection = await client.getDreamCollection();  
   await dreamCollection.deleteOne({ '_id': new ObjectId(id) });
   res.status(200).end();
+});
+
+app.get('/api/dream/weave', async(req, res) => {
+  const { id } = req.params;
+  const dreamCollection = await client.getDreamCollection();  
+  const dreams = await dreamCollection.find({}).sort({"date": -1}).limit(10).toArray();
+  let dreamString = dreams.map(d => d.summary).join('\n\n');
+
+  dreamString = 'Can you analyze these dreams and summarize in one paragraph what they might mean as a whole?\n\n' + dreamString; 
+  console.log(dreamString);
+
+  const completion = await openai.chat.completions.create({
+    messages: [
+      { role: "system", content: "You are a helpful assistant." },
+      {role: "user", content: dreamString},
+    ],
+    model: "gpt-3.5-turbo-16k",
+  });
+  res.json({ message: completion.choices[0].message.content });
 });
 
 app.listen(4700, () => {
